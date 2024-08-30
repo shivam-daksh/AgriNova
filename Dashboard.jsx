99b691d149134931f1ce3ec430bd4342cc38e08e
Shivam Daksh <shivamdakshshivam@gmail.com>
2024-08-30 12:58:12 +0530

    Change Soil Moisture to Moisture

--- Dashboard.jsx
+++ Dashboard.jsx
@@ -156,7 +156,7 @@ const Dashboard = () => {
         <Grid item xs={12} sm={4}>
           <Card>
             <CardContent>
-              <Typography variant="h6">Soil Moisture</Typography>
+              <Typography variant="h6">Moisture</Typography>
               <Typography variant="h4">
                 {isOnline ? soilMoisture : "Fetching..."}%
               </Typography>
