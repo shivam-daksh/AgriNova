754ad25cbeb7ce99596620058e6cb5f9118192b2
Shivam Daksh <shivamdakshshivam@gmail.com>
2024-08-31 09:31:13 +0530

    Adding chat interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Dashboard from "../Dashboard";
 import Dash from "../Board";
+import AIChatAssistant from "./AIChatAssistant";
 
 function App() {
   return (
     <>
       {/* <Dash /> */}
+      <AIChatAssistant />
       <Dashboard />
     </>
   );
